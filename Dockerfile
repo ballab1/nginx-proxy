da81569c3cf40537a954ac377aad5a1753863cda
Ballantyne, Bob <bob.ballantyne@emc.com>
2017-12-21 10:12:40 -0500

    fixed up new Docker layout

--- Dockerfile
+++ Dockerfile
@@ -1,51 +1,30 @@
 FROM alpine:3.6
 
-ARG TZ=UTC
-ARG user=nginx
-ARG group=nginx
-ARG uid=10777
-ARG gid=10777
+ARG TZ=America/New_York
+ARG nginx_user=nginx
+ARG nginx_group=nginx
+ARG nginx_uid=1001
+ARG nginx_gid=1001
+
+ENV NGINX_PKGS="bash nginx shadow openssl tzdata"
 
 #
 # PACKAGES
 #
-COPY etc_nginx.tgz /tmp/etc_nginx.tgz
-COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY build /tmp/ 
 
-RUN set -e \
+RUN set -o errexit \
+    \
     && apk update \
-    && apk add tzdata \
+    && apk add --no-cache $NGINX_PKGS \
+    \
     && echo $TZ > /etc/TZ \
     && cp /usr/share/zoneinfo/$TZ /etc/timezone \
     && cp /usr/share/zoneinfo/$TZ /etc/localtime \
-    && apk add --no-cache \
-            bash \
-            nginx \
-            shadow \
-            openssl \
-    && chmod u+rx,g+rx,o+rx,a-w /docker-entrypoint.sh \
-    && usermod -u ${uid} ${user} \
-    && groupmod -g ${gid} ${group} \
-    && mkdir -p /www \
-    && mkdir -p /opt/ssl \
-    && chown -R nginx:nginx /opt \
-    && chown -R nginx:nginx /var/log/nginx \
-    && mkdir -p /nginx/tmp \
-    && chown -R nginx:nginx /nginx \
-    && rm -rf /etc/nginx/* \
-    && tar -xvzf /tmp/etc_nginx.tgz -C /etc/nginx \
-    && mkdir -p /etc/nginx/ssl \
-    && cd /etc/nginx/ssl \
-    && openssl genrsa -des3 -passout pass:x -out server.pass.key 2048 \
-    && openssl rsa -passin pass:x -in server.pass.key -out server.key \
-    && rm server.pass.key \
-    && openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048 \
-    && openssl req -new -key server.key -out server.csr -subj "/C=US/ST=Massachusetts/L=Mansfield/O=ballantyne.io/OU=docker.nginx.io/CN=ubuntu-s3" \
-    && openssl x509 -req -sha256 -days 300065 -in server.csr -signkey server.key -out server.crt
-
-
-#RUN ln -sf /dev/stdout /var/log/nginx/access.log \
-#    && ln -sf /dev/stderr /var/log/nginx/error.log
+    \
+    && chmod u+rwx /tmp/build_container.sh \
+    && /tmp/build_container.sh \
+    && rm -rf /tmp/*
 
 #
 # RUN NGINX
@@ -53,7 +32,7 @@ RUN set -e \
 #USER nginx
 EXPOSE 80
 EXPOSE 443
-VOLUME ["/www"]
-WORKDIR /www/
+VOLUME ["/var/www"]
+WORKDIR /var/www/
 ENTRYPOINT ["/docker-entrypoint.sh"]
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["nginx"]
---
+++ build/build_container.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+#set -o xtrace
+set -o errexit
+set -o nounset 
+#set -o verbose
+
+declare TOOLS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  
+
+#directories
+declare WWW=/var/www 
+
+#  groups/users
+declare nginx_user=${nginx_user:-'nginx'}
+declare nginx_uid=${nginx_uid:-1001}
+declare nginx_group=${nginx_group:-'nginx'}
+declare nginx_gid=${nginx_gid:-1001} 
+declare www_user=${www_user:-'www-data'}
+declare www_uid=${www_uid:-82}
+declare www_group=${www_group:-'www-data'}
+declare www_gid=${www_gid:-82}
+
+# global exceptions
+declare -i dying=0
+declare -i pipe_error=0
+
+
+#----------------------------------------------------------------------------
+# Exit on any error
+function catch_error() {
+    echo "ERROR: an unknown error occurred at $BASH_SOURCE:$BASH_LINENO" >&2
+}
+
+#----------------------------------------------------------------------------
+# Detect when build is aborted
+function catch_int() {
+    die "${BASH_SOURCE[0]} has been aborted with SIGINT (Ctrl-C)"
+}
+
+#----------------------------------------------------------------------------
+function catch_pipe() {
+    pipe_error+=1
+    [[ $pipe_error -eq 1 ]] || return 0
+    [[ $dying -eq 0 ]] || return 0
+    die "${BASH_SOURCE[0]} has been aborted with SIGPIPE (broken pipe)"
+}
+
+#----------------------------------------------------------------------------
+function die() {
+    local status=$?
+    [[ $status -ne 0 ]] || status=255
+    dying+=1
+
+    printf "%s\n" "FATAL ERROR" "$@" >&2
+    exit $status
+}  
+
+#############################################################################
+function cleanup()
+{
+    printf "\nclean up\n"
+}
+
+
+#############################################################################
+function configureNGINX()
+{
+    usermod -u ${nginx_uid} ${nginx_user}
+    groupmod -g ${nginx_gid} ${nginx_group}
+
+    [[ -d /var/nginx/client_body ]] || mkdir -p /var/nginx/client_body
+    [[ -d /var/nginx/fastcgi_temp ]] || mkdir -p /var/nginx/fastcgi_temp
+    [[ -d /var/nginx/proxy_temp ]] || mkdir -p /var/nginx/proxy_temp
+    [[ -d /var/nginx/scgi_temp ]] || mkdir -p /var/nginx/scgi_temp
+    [[ -d /var/nginx/uwsgi_temp ]] || mkdir -p /var/nginx/uwsgi_temp
+    
+    mkdir -p "${WWW}"
+    mkdir -p /opt/ssl
+    mkdir -p /etc/nginx/ssl
+
+    echo ">> GENERATING SSL CERT"
+    cd /etc/nginx/ssl
+    openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+    openssl rsa -passin pass:x -in server.pass.key -out server.key
+    rm server.pass.key
+
+    openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+    openssl req -new -key server.key -out server.csr -subj "/C=US/ST=Massachusetts/L=Mansfield/O=ballantyne.io/OU=docker.nginx.io/CN=ubuntu-s3"
+    openssl x509 -req -sha256 -days 300065 -in server.csr -signkey server.key -out server.crt 
+    echo ">> GENERATING SSL CERT ... DONE"
+}
+
+#############################################################################
+function createUserAndGroup()
+{
+    local -r user=$1
+    local -r uid=$2
+    local -r group=$3
+    local -r gid=$4
+    local -r homedir=$5
+    local -r shell=$6
+    local result
+    
+    local wanted=$( printf '%s:%s' $group $gid )
+    local nameMatch=$( getent group "${group}" | awk -F ':' '{ printf "%s:%s",$1,$3 }' )
+    local idMatch=$( getent group "${gid}" | awk -F ':' '{ printf "%s:%s",$1,$3 }' )
+    printf "group/gid (%s):  is currently (%s)/(%s)\n" "$wanted" "$nameMatch" "$idMatch"           
+
+    if [[ $wanted != $nameMatch  ||  $wanted != $idMatch ]]; then
+        printf "\ncreate group:  %s\n" $group
+        [[ "$nameMatch"  &&  $wanted != $nameMatch ]] && groupdel "$( getent group ${group} | awk -F ':' '{ print $1 }' )"
+        [[ "$idMatch"    &&  $wanted != $idMatch ]]   && groupdel "$( getent group ${gid} | awk -F ':' '{ print $1 }' )"
+        /usr/sbin/groupadd --gid "${gid}" "${group}"
+    fi
+
+    
+    wanted=$( printf '%s:%s' $user $uid )
+    nameMatch=$( getent passwd "${user}" | awk -F ':' '{ printf "%s:%s",$1,$3 }' )
+    idMatch=$( getent passwd "${uid}" | awk -F ':' '{ printf "%s:%s",$1,$3 }' )
+    printf "user/uid (%s):  is currently (%s)/(%s)\n" "$wanted" "$nameMatch" "$idMatch"    
+    
+    if [[ $wanted != $nameMatch  ||  $wanted != $idMatch ]]; then
+        printf "create user: %s\n" $user
+        [[ "$nameMatch"  &&  $wanted != $nameMatch ]] && userdel "$( getent passwd ${user} | awk -F ':' '{ print $1 }' )"
+        [[ "$idMatch"    &&  $wanted != $idMatch ]]   && userdel "$( getent passwd ${uid} | awk -F ':' '{ print $1 }' )"
+
+        /usr/sbin/useradd --home-dir "$homedir" --uid "${uid}" --gid "${gid}" --no-create-home --shell "${shell}" "${user}"
+    fi
+}
+
+#############################################################################
+function installCUSTOMIZATIONS()
+{
+    printf "\nAdd configuration and customizations\n"
+    cp -r "${TOOLS}/etc"/* /etc
+    cp -r "${TOOLS}/usr"/* /usr
+    cp -r "${TOOLS}/var"/* /var
+
+    ln -s /usr/local/bin/docker-entrypoint.sh /docker-entrypoint.sh
+    
+    if [[ -h /var/lib/nginx/logs ]]; then
+        rm /var/lib/nginx/logs
+        ln -s /var/log /var/lib/nginx/logs
+    fi
+
+    [[ -d /sessions ]]    || mkdir -p /sessions
+    [[ -d /run/nginx ]]   || mkdir -p /run/nginx
+}
+
+
+#############################################################################
+function setPermissions()
+{
+    printf "\nmake sure that ownership & permissions are correct\n"
+
+#    chown root:root /etc/sudoers.d/*
+#    chmod 600 /etc/sudoers.d/*
+
+    chmod u+rx,g+rx,o+rx,a-w /usr/local/bin/docker-entrypoint.sh
+
+    chown "${nginx_user}":"${nginx_group}" -R /opt
+    chown "${nginx_user}":"${nginx_group}" -R /var/log/nginx
+    chown "${nginx_user}":"${nginx_group}" -R /var/nginx
+    chown "${www_user}":"${www_group}" -R "${WWW}"
+}
+
+
+#############################################################################
+
+trap catch_error ERR
+trap catch_int INT
+trap catch_pipe PIPE 
+
+set -o verbose
+
+#createUserAndGroup "${nginx_user}" "${nginx_uid}" "${nginx_group}" "${nginx_gid}" "${WWW}" /sbin/nologin
+createUserAndGroup "${www_user}" "${www_uid}" "${www_group}" "${www_gid}" "${WWW}" /sbin/nologin
+configureNGINX
+installCUSTOMIZATIONS
+setPermissions
+cleanup
+exit 0
